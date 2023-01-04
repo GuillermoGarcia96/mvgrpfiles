d88cb123e8b48eae3f5c7c7af00783d47559d22c
Guillermo <g.ggrao@upm.es>
2023-01-04 17:12:43 +0100

    feat: environment variable to decide the root directory to start looking for files

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pwd
 # TODO: handle exceptions
 # TODO: check process already running
 
+TOP_DIRECTORY = os.getenv("MVGRPFILES_TOP_DIRECTORY", default="/")
+
 def group_name_exists(group_name: str) -> bool:
     group_names = []
     for group in grp.getgrall():
@@ -14,15 +16,15 @@ def group_name_exists(group_name: str) -> bool:
     return group_name in group_names
 
 
-def find_files(group_name: str) -> dict[str, str]:
+def get_files_from_all_group_members(group_name: str) -> dict[str, str]:
 
     # Find all users in the group
     group_info = grp.getgrnam(group_name)
     users = group_info.gr_mem
 
-    file_names = []
+    group_files_by_user = {}
     for user in users:
-        file_names += get_files_owned_by_user("/", user)
+        group_files_by_user[user] = get_files_owned_by_user(TOP_DIRECTORY, user)
 
     # Find all files belonging to those users
     # file_names = []
@@ -43,20 +45,22 @@ def find_files(group_name: str) -> dict[str, str]:
     #             if file_stat.st_uid == pwd.getpwnam(user).pw_uid:
     #                 file_names.append(file_path)
 
-    return file_names
+    return group_files_by_user
 
 
 def get_files_owned_by_user(directory: str, user: str) -> list[str]:
     # Get the user's UID
     uid = pwd.getpwnam(user).pw_uid
 
-    # Get files in the directory owned by the user
+    # Get files owned by the user inside the directory
     file_names = []
     try:
         with os.scandir(directory) as iterable:
             for entry in iterable:
+
                 if entry.is_file() and os.stat(entry.path, follow_symlinks=False).st_uid == uid:
                     file_names.append(entry.path)
+
                 elif entry.is_dir() and not entry.is_symlink():
                     # Recursively check the subdirectory
                     file_names += get_files_owned_by_user(entry.path, user)
@@ -79,7 +83,9 @@ if __name__ == "__main__":
     if not group_name_exists(group_name):
         raise Exception()
 
-    file_names = find_files(group_name)
+    group_files_by_user = get_files_from_all_group_members(group_name)
 
     # print(files_without_permission)
-    print(file_names)
+    print(group_files_by_user)
+    for i in group_files_by_user.values():
+        print(len(i))
