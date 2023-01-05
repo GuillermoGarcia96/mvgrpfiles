c63054bec18ca64f36e54af9a2601efdd30bf754
Guillermo <g.ggrao@upm.es>
2023-01-05 17:14:59 +0100

    fix: scripts enhanced

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -m build
-python -m pip install dist/mvgrpfiles-0.0.0.tar.gz
+python3.9 -m build
+python3.9 -m pip install dist/mvgrpfiles-0.0.0.tar.gz
--- scripts/generate_test_files.sh
+++ scripts/generate_test_files.sh
@@ -3,7 +3,7 @@ for i in {1..500}
 do
     touch "/home/guillermo/test/file_${i}.txt"
     sudo chown user1:group1 "/home/guillermo/test/file_${i}.txt"
-    touch "/opt/data/test/file_${i}.txt"
+    sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user6:group3 "/opt/data/test/file_${i}.txt"
 done
 
@@ -11,7 +11,7 @@ for i in {500..1000}
 do
     touch "/home/guillermo/test/file_${i}.txt"
     sudo chown user2:group2 "/home/guillermo/test/file_${i}.txt"
-    touch "/opt/data/test/file_${i}.txt"
+    sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user7:group4 "/opt/data/test/file_${i}.txt"
 done
 
@@ -19,7 +19,7 @@ for i in {1000..1500}
 do
     touch "/home/guillermo/test/file_${i}.txt"
     sudo chown user3:group2 "/home/guillermo/test/file_${i}.txt"
-    touch "/opt/data/test/file_${i}.txt"
+    sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user8:group5 "/opt/data/test/file_${i}.txt"
 done
 
@@ -27,7 +27,7 @@ for i in {1500..2000}
 do
     touch "/home/guillermo/test/file_${i}.txt"
     sudo chown user4:group3 "/home/guillermo/test/file_${i}.txt"
-    touch "/opt/data/test/file_${i}.txt"
+    sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user9:group5 "/opt/data/test/file_${i}.txt"
 done
 
@@ -35,6 +35,6 @@ for i in {2000..3000}
 do
     touch "/home/guillermo/test/file_${i}.txt"
     sudo chown user5:group4 "/home/guillermo/test/file_${i}.txt"
-    touch "/opt/data/test/file_${i}.txt"
+    sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user10:group1 "/opt/data/test/file_${i}.txt"
 done
