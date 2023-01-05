c48155a45ff467e6fa2d15fdcedad4bf4b057a36
Guillermo <g.ggrao@upm.es>
2023-01-05 17:51:22 +0100

    feat: setup.py added back again

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
-import setuptools
-import toml
+from setuptools import setup
 
-with open("pyproject.toml") as f:
-    pyproject = toml.load(f)
-
-metadata = pyproject["metadata"]
-setuptools_requires = pyproject["setuptools_requires"]
-options = pyproject.get("options", {})
-entry_points = pyproject.get("entry_points", {})
-
-setuptools.setup(
-    name=metadata["name"],
-    version=metadata["version"],
-    author=metadata["author"],
-    author_email=metadata["author_email"],
-    description=metadata["description"],
-    long_description=metadata.get("long_description", ""),
-    long_description_content_type=metadata.get("long_description_content_type", ""),
-    url=metadata.get("url", ""),
-    classifiers=metadata.get("classifiers", []),
-    python_requires=metadata.get("python_requires", ""),
-    setuptools_requires=setuptools_requires,
-    packages=options.get("packages", []),
-    entry_points=entry_points,
+setup(
+    name='mvgrpfiles',
+    version='1.0',
+    description='A Python program that moves all files from all members of a group to an archive',
+    author='Guillermo García Grao',
+    author_email='guillermo.garcia.grao@gmail.com',
+    # py_modules=['groups', 'archive'],
+    entry_points={
+        'console_scripts': [
+            'mvgrpfiles = mvgrpfiles'
+        ]
+    },
 )
