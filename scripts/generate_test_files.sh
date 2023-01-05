67431d9f1c98eca86d0cf656d865406795cc4224
Guillermo <g.ggrao@upm.es>
2023-01-05 17:16:31 +0100

    fix: scripts enhanced

--- scripts/generate_test_files.sh
+++ scripts/generate_test_files.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 for i in {1..500}
 do
-    touch "/home/guillermo/test/file_${i}.txt"
-    chown user1:group1 "/home/guillermo/test/file_${i}.txt"
+    sudo touch "/home/guillermo/test/file_${i}.txt"
+    sudo chown user1:group1 "/home/guillermo/test/file_${i}.txt"
     sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user6:group3 "/opt/data/test/file_${i}.txt"
 done
 
 for i in {500..1000}
 do
-    touch "/home/guillermo/test/file_${i}.txt"
-    chown user2:group2 "/home/guillermo/test/file_${i}.txt"
+    sudo touch "/home/guillermo/test/file_${i}.txt"
+    sudo chown user2:group2 "/home/guillermo/test/file_${i}.txt"
     sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user7:group4 "/opt/data/test/file_${i}.txt"
 done
 
 for i in {1000..1500}
 do
-    touch "/home/guillermo/test/file_${i}.txt"
-    chown user3:group2 "/home/guillermo/test/file_${i}.txt"
+    sudo touch "/home/guillermo/test/file_${i}.txt"
+    sudo chown user3:group2 "/home/guillermo/test/file_${i}.txt"
     sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user8:group5 "/opt/data/test/file_${i}.txt"
 done
 
 for i in {1500..2000}
 do
-    touch "/home/guillermo/test/file_${i}.txt"
-    chown user4:group3 "/home/guillermo/test/file_${i}.txt"
+    sudo touch "/home/guillermo/test/file_${i}.txt"
+    sudo chown user4:group3 "/home/guillermo/test/file_${i}.txt"
     sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user9:group5 "/opt/data/test/file_${i}.txt"
 done
 
 for i in {2000..3000}
 do
-    touch "/home/guillermo/test/file_${i}.txt"
-    chown user5:group4 "/home/guillermo/test/file_${i}.txt"
+    sudo touch "/home/guillermo/test/file_${i}.txt"
+    sudo chown user5:group4 "/home/guillermo/test/file_${i}.txt"
     sudo touch "/opt/data/test/file_${i}.txt"
     sudo chown user10:group1 "/opt/data/test/file_${i}.txt"
 done
