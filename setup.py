53d118db5f3f5ff29d715760e4577ed7ef041657
Guillermo <g.ggrao@upm.es>
2023-01-05 11:30:17 +0100

    feat: setup.py ready for packaging

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='mvgrpfiles',
     version='1.0',
-    description='A simple Python program',
+    description='A Python program that moves all files from all members of a group to an archive',
     author='Guillermo García Grao',
     author_email='guillermo.garcia.grao@gmail.com',
-    py_modules=['groups', 'archive'],
-    entry_points={
-        'console_scripts': [
-            'mvgrpfiles = mvgrpfiles'
-        ]
-    },
+    # py_modules=['groups', 'archive'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'mvgrpfiles = mvgrpfiles'
+    #     ]
+    # },
 )
