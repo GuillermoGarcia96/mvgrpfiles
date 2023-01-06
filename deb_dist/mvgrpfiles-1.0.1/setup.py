dda6ce6d4f83c2614245de1ceba310b202fdbf3d
Guillermo <g.ggrao@upm.es>
2023-01-06 08:57:15 +0100

    fix: verbose logs fixed

---
+++ deb_dist/mvgrpfiles-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mvgrpfiles',
+    version='1.0.1',
+    description='A Python program that moves all files from all members of a group to an archive',
+    author='Guillermo García Grao',
+    author_email='guillermo.garcia.grao@gmail.com',
+    # py_modules=['groups', 'archive'],
+    entry_points={
+        'console_scripts': [
+            'mvgrpfiles = mvgrpfiles.__main__:main'
+        ]
+    },
+)
