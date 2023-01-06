3253a0381b6010547673c53346e25a565cfdc084
Guillermo <g.ggrao@upm.es>
2023-01-06 12:02:38 +0100

    test: tests added for the validators

--- tests/test_fs_interface.py
+++ tests/test_fs_interface.py
@@ -4,7 +4,7 @@ import unittest
 
 from mvgrpfiles.fs_interface import *
 
-class TestCreateLock(unittest.TestCase):
+class TestLocks(unittest.TestCase):
     def test_create_lock(self):
         # Test that create_lock creates a lock file
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -12,21 +12,6 @@ class TestCreateLock(unittest.TestCase):
             create_lock(lock_path)
             self.assertTrue(os.path.exists(lock_path))
 
-    def test_overwrite_lock(self):
-        # Test that create_lock overwrites an existing lock file
-        with tempfile.TemporaryDirectory() as tmpdir:
-            lock_path = os.path.join(tmpdir, 'lock')
-            with open(lock_path, 'w'):
-                pass
-            create_lock(lock_path)
-            self.assertTrue(os.path.exists(lock_path))
-
-    def test_invalid_lock_path(self):
-        # Test that create_lock raises an OSError for an invalid lock path
-        with self.assertRaises(OSError):
-            create_lock('/invalid/lock/path')
-
-class TestRemoveLock(unittest.TestCase):
     def test_remove_lock(self):
         # Test that remove_lock removes a lock file
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -36,19 +21,6 @@ class TestRemoveLock(unittest.TestCase):
             remove_lock(lock_path)
             self.assertFalse(os.path.exists(lock_path))
 
-    def test_remove_nonexistent_lock(self):
-        # Test that remove_lock raises an OSError for a nonexistent lock file
-        with tempfile.TemporaryDirectory() as tmpdir:
-            lock_path = os.path.join(tmpdir, 'lock')
-            with self.assertRaises(OSError):
-                remove_lock(lock_path)
-
-    def test_invalid_lock_path(self):
-        # Test that remove_lock raises an OSError for an invalid lock path
-        with self.assertRaises(OSError):
-            remove_lock('/invalid/lock/path')
-
-class TestGetLockedGroups(unittest.TestCase):
     def test_get_locked_groups(self):
         # Test that get_locked_groups returns a list of locked groups
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -70,7 +42,15 @@ class TestGetLockedGroups(unittest.TestCase):
             locked_groups = get_locked_groups(locks_path)
             self.assertEqual(locked_groups, [])
 
-    def test_nonexistent_locks_path(self):
+    def test_invalid_lock_path(self):
+        # Test that create_lock raises an OSError for an invalid lock path
+        with self.assertRaises(OSError):
+            create_lock('/invalid/lock/path')
+
+        # Test that remove_lock raises an OSError for an invalid lock path
+        with self.assertRaises(OSError):
+            remove_lock('/invalid/lock/path')
+
         # Test that get_locked_groups raises an OSError for a nonexistent locks path
         with self.assertRaises(OSError):
             get_locked_groups('/nonexistent/locks/path')
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,8 +1,44 @@
 import os
-import tempfile
 import unittest
+import shutil
 
 from mvgrpfiles.validators import *
+
+class TestValidators(unittest.TestCase):
+    def test_group_name_exists(self):
+        # Test group that exists
+        assert group_name_exists('root')
+
+        # Test group that does not exist
+        assert not group_name_exists('fake_group')
+
+    def test_groups_share_members(self):
+        # Test groups with no shared members
+        assert not groups_share_members('group1', 'group2')
+
+        # Test groups with shared members
+        assert groups_share_members('group1', 'group4')
+
+    def test_validate_directories_exist(self):
+        # Create a temporary directory for testing
+        temp_dir = 'temp'
+        os.makedirs(temp_dir, mode=0o766)
+
+        # Test directories that do not exist
+        validate_directories_exist([temp_dir + '/dir1', temp_dir + '/dir2', temp_dir + '/dir3'])
+        assert os.path.exists(temp_dir + '/dir1')
+        assert os.path.exists(temp_dir + '/dir2')
+        assert os.path.exists(temp_dir + '/dir3')
+
+        # Test directories that already exist
+        validate_directories_exist([temp_dir + '/dir1', temp_dir + '/dir2', temp_dir + '/dir3'])
+        assert os.path.exists(temp_dir + '/dir1')
+        assert os.path.exists(temp_dir + '/dir2')
+        assert os.path.exists(temp_dir + '/dir3')
+
+        # Clean up
+        shutil.rmtree(temp_dir)
+
 
 if __name__ == '__main__':
     unittest.main()
